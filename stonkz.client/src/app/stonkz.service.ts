470c4885cce64b5c940c8f9037bf3898a64029b1
MONOLITH-MK2\ViktorZin <viktorzin@viktorzin.com>
2025-07-03 13:27:24 +0200

    Loading the Data into the Service seems to work, and also Dictionary Generation seems to work fine for now, it triggers right after the StonkzData Array is loaded.

--- stonkz.client/src/app/stonkz.service.ts
+++ stonkz.client/src/app/stonkz.service.ts
@@ -3,33 +3,48 @@ import { HttpClient } from '@angular/common/http';
 import { StonkzData } from './Interfaces/stonkz-data'
 import { Stonk } from './Interfaces/stonk';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class StonkzService {
 
+  //stonkData: StonkzData[] = [];
+  //stonkDataDict: StonkDataDict = {0, []}
+  stonkDataDict: Record<number, StonkzData[]> = {};
   stonkData: StonkzData[] = [];
+  stonkDataPromise: Promise<StonkzData[]> | null = null;
   stonkz: Stonk[] = [];
   loadingPromise: Promise<Stonk[]> | null = null;
   constructor(private http: HttpClient)
   {
     this.loadStonkz();
+    this.loadStonkData().then(() => {
+      console.log("loadingStonkData finished. calling dict generation");
+      this.generateStonkDataDictionary();
+    })
   }
 
-  /*
-  loadStonkz() {
-    console.log("trying to load stonkz");
-    this.http.get<Stonk[]>('/stonk').subscribe(
-      (result) => {
-        this.stonkz = result;
-      },
-      (error) => {
-        console.error(error + " SO EIN ERROR JUNGE");
+  public debugLogArrayLength() {
+    console.log("Stonkz Array Length: " + this.stonkz.length);
+    console.log("StonkData Array Length: " + this.stonkData.length);
+  }
+
+  private generateStonkDataDictionary() {
+    console.log("in dict generation. starting.");
+    for (let i = 0; i < this.stonkz.length; i++) {
+      if (this.stonkz[i].stonkId in this.stonkDataDict) {
+        //key Exists
+        continue;
       }
-    );
-    console.log("should have loded Stonks. Stonk length: " + this.stonkz.length);
-    //return this.stonkz;
-  }*/
+      else {
+        //key does not Exist.
+        this.stonkDataDict[this.stonkz[i].stonkId] = this.stonkData.filter(d => d.stonkId === this.stonkz[i].stonkId);
+      }
+    }
+    console.log("done with dict generation...");
+  }
+
 
   private loadStonkz(): Promise<Stonk[]> {
     if (this.loadingPromise) {
@@ -51,13 +66,34 @@ export class StonkzService {
     return this.loadingPromise;
   }
 
-  public async getStonkz(): Promise<Stonk[]> {
-    if (this.stonkz.length > 0) {
-      return this.stonkz;
+  private loadStonkData(): Promise<StonkzData[]> {
+    if (this.stonkDataPromise) {
+      return this.stonkDataPromise;
     }
 
-    return await this.loadStonkz();
+    this.stonkDataPromise = new Promise<StonkzData[]>((resolve, reject) => {
+      this.http.get<StonkzData[]>('/stonkdata').subscribe({
+        next: (data) => {
+          this.stonkData = data;
+          resolve(data);
+        },
+        error: (err) => {
+          console.error("StonkData Loading Error: ", err);
+          reject(err);
+        }
+      });
+    });
+    return this.stonkDataPromise;
   }
+
+
+public async getStonkz(): Promise<Stonk[]> {
+  if (this.stonkz.length > 0) {
+    return this.stonkz;
+  }
+
+  return await this.loadStonkz();
+}
 
   /*
   public getStonkz() {
@@ -67,7 +103,7 @@ export class StonkzService {
     return this.stonkz;
   }*/
 
-  public getStonkData(stonkId: number, toDate: Date): StonkzData[] {
+  //public getStonkData(stonkId: number, toDate: Date): StonkzData[] {
     //this.http.get<StonkzData[]>('/stonkdata/' + stonkId + "/" + toDate.getFullYear()
     //  + "/" + toDate.getMonth() + "/" + toDate.getDate()).subscribe(
     //    (result) => {
@@ -77,7 +113,7 @@ export class StonkzService {
     //      console.error(error);
     //    }
     //);
-
+    /*
     this.http.get<StonkzData[]>('/stonkdata/' + 5).subscribe(
         (result) => {
           this.stonkData = result;
@@ -88,5 +124,6 @@ export class StonkzService {
       );
 
     return this.stonkData;
-  }
+    */
+ // }
 }
