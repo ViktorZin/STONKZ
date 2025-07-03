889f886ab4f7e749c71a3232e4edf478ce4a0bb4
MONOLITH-MK2\ViktorZin <viktorzin@viktorzin.com>
2025-07-03 12:22:29 +0200

    Managed to make an async call work using promises for retrieving Data from the Server.

--- stonkz.client/src/app/stonkz.service.ts
+++ stonkz.client/src/app/stonkz.service.ts
@@ -6,17 +6,17 @@ import { Stonk } from './Interfaces/stonk';
 @Injectable({
   providedIn: 'root'
 })
-export class StonkzService implements OnInit {
+export class StonkzService {
 
   stonkData: StonkzData[] = [];
   stonkz: Stonk[] = [];
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {
-    //this.getStonkz();
+  loadingPromise: Promise<Stonk[]> | null = null;
+  constructor(private http: HttpClient)
+  {
     this.loadStonkz();
   }
 
+  /*
   loadStonkz() {
     console.log("trying to load stonkz");
     this.http.get<Stonk[]>('/stonk').subscribe(
@@ -29,14 +29,43 @@ export class StonkzService implements OnInit {
     );
     console.log("should have loded Stonks. Stonk length: " + this.stonkz.length);
     //return this.stonkz;
+  }*/
+
+  private loadStonkz(): Promise<Stonk[]> {
+    if (this.loadingPromise) {
+      return this.loadingPromise; 
+    }
+
+    this.loadingPromise = new Promise<Stonk[]>((resolve, reject) => {
+      this.http.get<Stonk[]>('/stonk').subscribe({
+        next: (data) => {
+          this.stonkz = data;
+          resolve(data);
+        },
+        error: (err) => {
+          console.error("Stonkz Loading Error: ", err);
+          reject(err);
+        }
+      });
+    });
+    return this.loadingPromise;
   }
 
+  public async getStonkz(): Promise<Stonk[]> {
+    if (this.stonkz.length > 0) {
+      return this.stonkz;
+    }
+
+    return await this.loadStonkz();
+  }
+
+  /*
   public getStonkz() {
     if (this.stonkz.length === 0) {
       this.loadStonkz();
     }
     return this.stonkz;
-  }
+  }*/
 
   public getStonkData(stonkId: number, toDate: Date): StonkzData[] {
     //this.http.get<StonkzData[]>('/stonkdata/' + stonkId + "/" + toDate.getFullYear()
