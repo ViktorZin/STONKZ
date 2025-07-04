36d1da7dd24b79e2c0fbb706055018a973a7eb3a
MONOLITH-MK2\ViktorZin <viktorasd@gmx.de>
2025-07-04 19:06:56 +0200

    Added OwnedStonkz and UserData C# classes (Models) as well as a new UserDataContext class for Database mapping. Added UserDataController and OwnedStonkzController for database conmmunication. added a new Migration and updated the Database. started working on UserData. Posted the initial Data from Angular to the Database. trying to Read and load it from Database now. it works, I can read it but then have trouble working with it in Angular. will fix next time.

---
+++ STONKZ.Server/Migrations/UserData/20250704164003_UserDatahotfix.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace STONKZ.Server.Migrations.UserData
+{
+    /// <inheritdoc />
+    public partial class UserDatahotfix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OwnedStonkzs_UserDatas_StonkId",
+                table: "OwnedStonkzs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OwnedStonkzs_StonkId",
+                table: "OwnedStonkzs");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserDataId",
+                table: "OwnedStonkzs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OwnedStonkzs_UserDataId",
+                table: "OwnedStonkzs",
+                column: "UserDataId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OwnedStonkzs_UserDatas_UserDataId",
+                table: "OwnedStonkzs",
+                column: "UserDataId",
+                principalTable: "UserDatas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OwnedStonkzs_UserDatas_UserDataId",
+                table: "OwnedStonkzs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OwnedStonkzs_UserDataId",
+                table: "OwnedStonkzs");
+
+            migrationBuilder.DropColumn(
+                name: "UserDataId",
+                table: "OwnedStonkzs");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OwnedStonkzs_StonkId",
+                table: "OwnedStonkzs",
+                column: "StonkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OwnedStonkzs_UserDatas_StonkId",
+                table: "OwnedStonkzs",
+                column: "StonkId",
+                principalTable: "UserDatas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
